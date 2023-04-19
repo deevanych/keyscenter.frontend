c48ea0ad4f9c512d6e9c562eb660ba6574eef031
deevanych <deevanych@gmail.com>
2023-04-20 00:03:07 +0300

    update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+// @ts-ignore
     runtimeConfig: {
         public: {
             backendUrl: process.env.BACKEND_URL || ''

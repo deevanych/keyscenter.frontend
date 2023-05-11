87660d900ea65086755be971ef58297966d66bc5
deevanych <deevanych@gmail.com>
2023-05-11 11:39:58 +0300

    add yandex metrika

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,28 @@ export default defineNuxtConfig({
                     name: 'yandex-verification',
                     content: '499b3cbff2687c7b'
                 }
+            ],
+            script: [
+                {
+                    innerHTML: '   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};\n' +
+                        '   m[i].l=1*new Date();\n' +
+                        '   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}\n' +
+                        '   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})\n' +
+                        '   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");\n' +
+                        '\n' +
+                        '   ym(' + process.env.YANDEX_METRIKA_COUNTER_ID + ', "init", {\n' +
+                        '        clickmap:true,\n' +
+                        '        trackLinks:true,\n' +
+                        '        accurateTrackBounce:true,\n' +
+                        '        webvisor:true,\n' +
+                        '        ecommerce:"dataLayer"\n' +
+                        '   });\n'
+                }
+            ],
+            noscript: [
+                {
+                    innerHTML: '<div><img src="https://mc.yandex.ru/watch/' + process.env.YANDEX_METRIKA_COUNTER_ID + '" style="position:absolute; left:-9999px;" alt="" /></div>'
+                }
             ]
         }
     },
@@ -29,7 +51,7 @@ export default defineNuxtConfig({
         }
     },
     css: [
-      '~/assets/scss/main.scss'
+        '~/assets/scss/main.scss'
     ],
     postcss: {
         plugins: {
@@ -44,9 +66,9 @@ export default defineNuxtConfig({
     ],
     robots: {
         rules: {
-                UserAgent: '*',
-                Allow: '/',
-                Host: process.env.BASE_URL
-            }
+            UserAgent: '*',
+            Allow: '/',
+            Host: process.env.BASE_URL
+        }
     }
 })

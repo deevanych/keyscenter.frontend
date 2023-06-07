c8bbf32ee054dbd4c7bfb5c46c6fd76864c6b362
deevanych <deevanych@gmail.com>
2023-06-08 02:49:50 +0300

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
 		head: {
 			charset: 'utf-8',
 			viewport: 'width=device-width, initial-scale=1',
-			titleTemplate: '🗝️ %s - Магазин цифровых ключей',
+			titleTemplate: '%s - Магазин цифровых ключей',
 			link: [
 				...faviconLinks()
 			],

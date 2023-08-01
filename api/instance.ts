616a1c1284cba000a7226196d4ef70b80fef0145
deevanych <deevanych@gmail.com>
2023-08-01 20:47:03 +0300

    fix requests error handling

--- api/instance.ts
+++ api/instance.ts
@@ -1,13 +1,12 @@
-import { $fetch } from "ofetch";
-import { URLHelpers } from "~/helpers/URL";
-import { HTTPMethod } from "h3";
+import {$fetch} from "ofetch";
+import {URLHelpers} from "~/helpers/URL";
+import {HTTPMethod} from "h3";
 
 export const $API = async (url = '', params = {}, method: HTTPMethod = "GET", body: undefined | any = undefined) => {
-	return await $fetch(url, {
-		body,
-		method,
-		baseURL: URLHelpers.getBackendURLHref('/api/'),
-		params,
-		ignoreResponseError: true
-	})
+    return await $fetch(url, {
+        body,
+        method,
+        baseURL: URLHelpers.getBackendURLHref('/api/'),
+        params
+    })
 }
